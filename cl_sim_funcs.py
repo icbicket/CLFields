7b18d4e75c05be026a69aebfd13ac8256d1972f1
icbicket <icbicket@users.noreply.github.com>
2019-06-13 20:56:26 -0400

    Added functions from jupyter notebook to cl_sim_funcs

--- cl_sim_funcs.py
+++ cl_sim_funcs.py
@@ -5,6 +5,115 @@ from mpl_toolkits import mplot3d
 from matplotlib import cm
 import matplotlib as mpl
 
+def ar_mask_calc(theta, phi, holein=True, slit=None, orientation=0):
+    
+    #solve equality ar^2-1/(4a)=x. 
+    #c=1/(2*(a*cos(phi)*sin(theta)+sqrt(a^2*(cos(theta)^2+(cos(phi)^2+sin(phi)^
+    #2)*sin(theta)^2)))); The cos(phi)^2+sin(phi)^2=1 so we can omit that. Than
+    #we have cos(theta)^2+sin(theta)^2 which also drops out. That leaves the
+    #square root of a^2 
+    
+    a = 0.1
+    xcut = 10.75
+    phi = phi + orientation
+
+    ##thetacutoff can actually be calculated
+    holesize = 0.6
+    holeheight = np.sqrt(2.5/a)
+#    thetacutoffhole=np.arctan(holesize/(2*holeheight))*180/np.pi
+    thetacutoffhole = 4
+    dfoc = 0.5
+
+#    phi,theta=np.meshgrid(phi1,theta1) ##Toon
+    c = 1./(2*(a*np.cos(phi)*np.sin(theta)+a))
+
+    z = np.cos(theta)*c
+    x = np.sin(theta)*np.cos(phi)*c#-1/(4.*a)
+    y = np.sin(theta)*np.sin(phi)*c
+    
+    condition = (-x > xcut) | (z < dfoc)
+    
+    if slit is not None:
+        ycut = slit/2.
+        condition = (condition | (np.abs(y) > ycut))
+    else:
+        pass
+
+    if holein is True:
+        condition = (condition | (theta < (thetacutoffhole*np.pi/180)))
+    else:
+        pass
+
+    mask = np.ones(np.shape(phi))
+    mask[condition] = False
+        
+    return mask  
+
+def ar_plot(theta, phi, magnitude, lim=(0,1)):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='polar')
+    scatterplot = ax.scatter(
+        phi, 
+        np.degrees(theta),
+        c = np.real(magnitude),
+        s=100,
+        vmin=lim[0],
+        vmax=lim[1])
+    ax.set_rmax(90)
+    ax.set_theta_zero_location('N')
+    plt.colorbar(scatterplot, ax=ax)
+    return fig
+
+def cartesian_to_spherical_coords(vectors):
+    r = np.sqrt(np.sum(np.square(vectors), axis=-1))
+    theta = np.arctan(vectors[:, 1]/vectors[:, 2])
+    phi = np.arccos(vectors[:, 0]/r
+    phi[(vectors[:, 1] > 0) * (vectors[:, 0] < 0)] += np.pi # +y, -z
+    phi[(vectors[:, 1] < 0) * (vectors[:, 0] > 0)] += 2*np.pi #-y, +z
+    phi[(vectors[:, 1] < 0) * (vectors[:, 0] < 0)] += np.pi # -y, -z
+    return r, theta, phi
+
+def cartesian_to_spherical_vector_field(theta, phi, fx, fy, fz):
+    fr = (np.sin(theta) * np.cos(phi) * fx + 
+       np.sin(theta) * np.sin(phi) * fy + 
+       np.cos(theta) * fz)
+e_thz = (np.cos(theta) * np.cos(phi) * fx + 
+        np.cos(theta) * np.sin(phi) * fy - 
+        np.sin(theta) * fz)
+e_phz = (-np.sin(phi) * fx + 
+        np.cos(phi) * fy)
+
+def degree_of_polarization(S0, S1, S2, S3):
+    DoP = np.sqrt(np.square(S1) + np.square(S2) + np.square(S3))/S0
+    DoLP = np.sqrt(np.square(S1) + np.square(S2))/S0
+    DoCP = S3/S0
+    ellipticity = S3/(S0 + np.sqrt(np.square(S1) + np.square(S2)))
+    return DoP, DoLP, DoCP, ellipticity
+
+def expand_quadrant_symmetry(mag):
+    Q1 = np.average(np.real(mag), axis=0):
+    Q2 = np.flip(Q1, axis=1)[:, :-1]
+    Q3 = np.flip(Q1, axis=0)[:-1, :]
+    Q4 = np.flip(np.flip(Q1, axis=0), axis=1)[:-1, :-1]
+    Q12 = np.append(Q2, Q1, axis=1)
+    Q34 = np.append(Q4, Q3, axis=1)
+    total = np.append(Q34, Q12, axis=0)
+    return total
+
+def field_magnitude(f):
+    f_mag = np.sqrt(np.sum(f * np.conj(f), axis=-1))
+    f_mag = np.real(f)
+    return f_mag
+
+def mirror_mask(theta, phi, slit=None, orientation=0):
+    mirror = np.logical_not(ar_mask_calc(theta, phi, slit=slit, orientation=orientation)
+    mirror = np.expand_dims(mirror, axis=1)
+    mirror = np.zeros(np.shape(mirror)).astype('bool')
+    mirror_3 = np.expand_dims(mirror, axis=1)
+    mirror_3 = np.repeat(mirror_f, 3, axis=1)
+    mirror_3 = np.repeat(mirror_f, 3, axis=2)
+    return mirror, mirror_3
+
 def norm_colours(values, lims=(-1,1)):
     # Normalize colour values to between 0 and 1
     maxcol = np.max(np.abs(values))
@@ -66,3 +175,9 @@ def plot_3d_fields(mesh, colour, ax=None, cmap=cm.seismic, col_scale=(-1,1)):
     plt.show()
     return fig
 
+def stokes_parameters(E_theta, E_phi):
+    S0 = np.real(E_theta * np.conj(E_theta) + E_phi * np.conj(E_phi))
+    S1 = np.real(E_theta * np.conj(E_theta) - E_phi * np.conj(E_phi))
+    S2 = 2 * np.real(E_theta * np.conj(E_phi))
+    S3 = -2 * np.imag(E_theta * np.conj(E_phi))
+    return S0, S1, S2, S3
