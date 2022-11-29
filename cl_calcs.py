Adding mueller matrix calculation for an ellipsoidal mirror and fixing a complex number casting bug in the reflection coefficient calculation

cl_calcs.py
@@ -95,11 +95,8 @@ def reflection_coefficients(incidence_angle, n_surface, n_environment=1):
     if not isinstance(incidence_angle, np.ndarray):
         incidence_angle = np.array([incidence_angle])
     refraction_angle = snells_law(incidence_angle, n_surface, n_environment)
-    r_s = np.empty(np.shape(incidence_angle))
-    r_p = np.empty(np.shape(incidence_angle))
-    if isinstance(n_surface, complex) or isinstance(n_environment, complex):
-        r_s = r_s.astype(complex)
-        r_p = r_p.astype(complex)
+    r_s = np.empty(np.shape(incidence_angle), dtype=np.complex64)
+    r_p = np.empty(np.shape(incidence_angle), dtype=np.complex64)
     r_s[incidence_angle==0] = (
         (n_environment - n_surface)/(n_surface + n_environment)
         )
@@ -191,3 +188,4 @@ def wavelength_to_eV(wavelength):
     '''
     eV = constants.PLANCK * constants.LIGHTSPEED / (wavelength * constants.COULOMB)
     return eV
+

cl_calcs_test.py
@@ -731,6 +731,49 @@ class ReflectionCoefficientsTest(parameterized.TestCase):
         np.testing.assert_allclose(expected_r_p, calculated_r_p, atol=1e-6)
 
 
+    def test_numpy_array_refractive_index(self):
+        '''
+        test the fresnel reflection coefficients for aluminium as the surface,
+        at different angles of incidence
+        Values obtained from Wolfram demo project at (noting this is using 
+        optical sign convention - switch the sign of r_p):
+             Tayari Colemanand Anna Petrova-Mayor 
+             "Fresnel Coefficients of Metals"
+             http://demonstrations.wolfram.com/FresnelCoefficientsOfMetals/
+             Wolfram Demonstrations Project
+             Published: August 31, 2020 
+        '''
+        n_environment = 1
+        n_surface = np.array([0.965+6.399j])
+        incidence_angle = np.deg2rad(np.array(
+            [0, 10, 20, 30, 40, 50, 60, 70, 80]))
+        expected_r_p = np.array([
+            -0.912293 - 0.285616j,
+            -0.910291 - 0.289706j,
+            -0.903869 - 0.302525j,
+            -0.891575 - 0.325905j,
+            -0.870092 - 0.363598j,
+            -0.831861 - 0.422767j,
+            -0.757610 - 0.517044j,
+            -0.587749 - 0.669632j,
+            -0.0914527 - 0.853458j
+            ])
+        expected_r_s = np.array([
+            -0.912293 - 0.285616j,
+            -0.914249 - 0.281575j,
+            -0.919947 - 0.269512j,
+            -0.928898 - 0.249618j,
+            -0.940339 - 0.222237j,
+            -0.953310 - 0.187908j,
+            -0.966749 - 0.147392j,
+            -0.979601 - 0.101696j,
+            -0.990928 - 0.0520749j
+            ])
+        calculated_r_s, calculated_r_p = cl_calcs.reflection_coefficients(
+            incidence_angle, n_surface, n_environment)
+        np.testing.assert_allclose(expected_r_s, calculated_r_s, atol=1e-6)
+        np.testing.assert_allclose(expected_r_p, calculated_r_p, atol=1e-6)
+
 class ReflectedETest(parameterized.TestCase):
     '''
     Test the calculation of the reflected electric field
@@ -984,7 +1027,7 @@ class NormalizeStokesParametersTest(unittest.TestCase):
             )
 
 
-class DielectricToRefraciveTest(parameterized.TestCase):
+class DielectricToRefractiveTest(parameterized.TestCase):
     '''
     normal dielectric function
     no imaginary component

mirror_operations.py
@@ -339,7 +339,6 @@ def get_mirror_reflected_field(incident_direction, incident_e, wavelength, n_env
     parabola_positions = parabola_position(incident_direction)
     surface_normal = parabola_normals(parabola_positions)
     n_surface = get_mirror_refractive_index(wavelength, mirror=mirror, **kwargs)
-    print(np.shape(incident_direction), np.shape(incident_e), np.shape(surface_normal), np.shape(n_surface), np.shape(n_environment))
     reflected_e_s, reflected_e_p = clc.reflected_e(
         incident_direction,
         incident_e,
@@ -348,3 +347,52 @@ def get_mirror_reflected_field(incident_direction, incident_e, wavelength, n_env
         n_environment=n_environment
         )
     return reflected_e_s, reflected_e_p
+
+
+def mueller_matrix_ellipsoidal(e_h_h, e_h_v, e_v_h, e_v_v):
+    '''
+    Calculate the mueller matrix for an ellipsoidal mirror
+    Rodriguez-Herrera and Bruce 2006
+    Input: 
+    - e_h_h, electric field with horizontal input polarization and horizontal output polarization
+    - e_h_v, input horizontal, output vertical polarized light
+    - e_v_h, input vertical, output horizontal
+    - e_v_v, input vertical, output vertical
+    '''
+    m00 = 0.5 * (
+          e_h_h * np.conj(e_h_h) 
+        + e_h_v * np.conj(e_h_v) 
+        + e_v_h * np.conj(e_v_h)
+        + e_v_v * np.conj(e_v_v)
+        )
+    m01 = 0.5 * (
+          e_h_h * np.conj(e_h_h)
+        + e_h_v * np.conj(e_h_v)
+        - e_v_h * np.conj(e_v_h)
+        - e_v_v * np.conj(e_v_v)
+        )
+    m02 = np.real(e_h_h * np.conj(e_v_h)) + np.real(e_h_v * np.conj(e_v_v))
+    m03 = np.imag(e_h_v * np.conj(e_v_v)) + np.imag(e_h_h * np.conj(e_v_h))
+    m10 = 0.5 * (
+          e_h_h * np.conj(e_h_h)
+        - e_h_v * np.conj(e_h_v)
+        + e_v_h * np.conj(e_v_h)
+        - e_v_v * np.conj(e_v_v)
+        )
+    m11 = 0.5 * (
+          e_h_h * np.conj(e_h_h)
+        - e_h_v * np.conj(e_h_v)
+        - e_v_h * np.conj(e_v_h)
+        + e_v_v * np.conj(e_v_v)
+        )
+    m12 = np.real(e_h_h * np.conj(e_v_h)) - np.real(e_h_v * np.conj(e_v_v))
+    m13 = np.imag(e_h_h * np.conj(e_v_h)) - np.imag(e_h_v * np.conj(e_v_v))
+    m20 = np.real(e_h_h * np.conj(e_h_v)) + np.real(e_v_h * np.conj(e_v_v))
+    m21 = np.real(e_h_h * np.conj(e_h_v)) - np.real(e_v_h * np.conj(e_v_v))
+    m22 = np.real(e_h_h * np.conj(e_v_v)) + np.real(e_v_h * np.conj(e_h_v))
+    m23 = np.imag(e_h_h * np.conj(e_v_v)) - np.imag(e_v_h * np.conj(e_h_v))
+    m30 = -np.imag(e_h_h * np.conj(e_h_v)) - np.imag(e_v_h * np.conj(e_v_v))
+    m31 = -np.imag(e_h_h * np.conj(e_h_v)) + np.imag(e_v_h * np.conj(e_v_v))
+    m32 = -np.imag(e_h_h * np.conj(e_v_v)) - np.imag(e_v_h * np.conj(e_h_v))
+    m33 = np.real(e_h_h * np.conj(e_v_v)) - np.real(e_v_h * np.conj(e_h_v))
+    return m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33

mirror_operations_test.py
@@ -1946,18 +1946,10 @@ class MirrorRefractiveIndexTest(parameterized.TestCase):
 class MirrorReflectedFieldTest(parameterized.TestCase):
     '''
     Testing the calculation of electric field vectors reflected off the mirror
-    - same shape
-    - normal incidence should return the same vector
-    - reflection should flip the sign of p-pol??
+    - output is the same shape as the input
+    - 
     '''
-#    @parameterized.named_parameters(
-#        dict(testcase_name='',
-#             ,
-#            ),
-#        dict(testcase_name='',
-#             ,
-#            ),
-#        )
+    
     def testOutputShape(self):
         '''
         Check the shape of the output field is the same as the shape of the input field
@@ -1975,6 +1967,104 @@ class MirrorReflectedFieldTest(parameterized.TestCase):
         expected_reflected_e_shape = np.shape(incident_direction)
         np.testing.assert_equal(np.shape(calculated_reflected_e_s), expected_reflected_e_shape)
         np.testing.assert_equal(np.shape(calculated_reflected_e_p), expected_reflected_e_shape)
+
+
+class MuellerMatrixEllipsoidalTest(parameterized.TestCase):
+    '''
+    Testing the Mueller matrix calculation for an ellipsoidal mirror
+    - all zeros
+    - all real ones
+    - all imaginary ones
+    - mixed real + imaginary ones
+    - arbitrary values
+    '''
+    @parameterized.named_parameters(
+        dict(testcase_name='All zeros',
+             e_h_h = 0,
+             e_h_v = 0,
+             e_v_h = 0,
+             e_v_v = 0,
+             expected_m = (
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0)
+            ),
+        dict(testcase_name='real ones',
+             e_h_h = 1,
+             e_h_v = 1,
+             e_v_h = 1,
+             e_v_v = 1,
+             expected_m = (
+                2, 0, 2, 0,
+                0, 0, 0, 0,
+                2, 0, 2, 0,
+                0, 0, 0, 0
+                )
+            ),
+        dict(testcase_name='Imaginary ones',
+             e_h_h = 1j,
+             e_h_v = 1j,
+             e_v_h = 1j,
+             e_v_v = 1j,
+             expected_m = (
+                2, 0, 2, 0,
+                0, 0, 0, 0,
+                2, 0, 2, 0,
+                0, 0, 0, 0
+                )
+            ),
+        dict(testcase_name='Mixed real and imag ones positive',
+             e_h_h = 1+1j,
+             e_h_v = 1+1j,
+             e_v_h = 1+1j,
+             e_v_v = 1+1j,
+             expected_m = (
+                4, 0, 4, 0,
+                0, 0, 0, 0,
+                4, 0, 4, 0,
+                0, 0, 0, 0)
+            ),
+        dict(testcase_name='Mixed real and imag ones negative',
+             e_h_h = -1-1j,
+             e_h_v = -1-1j,
+             e_v_h = -1-1j,
+             e_v_v = -1-1j,
+             expected_m = (
+                4, 0, 4, 0,
+                0, 0, 0, 0,
+                4, 0, 4, 0,
+                0, 0, 0, 0)
+            ),
+        dict(testcase_name='Mixed real and imag ones mixed case',
+             e_h_h = -1+1j,
+             e_h_v = +1-1j,
+             e_v_h = +1+1j,
+             e_v_v = -1-1j,
+             expected_m = (
+                4, 0, 0, 4,
+                0, 0, 0, 0,
+                -4, 0, 0, -4,
+                0, 0, 0, 0)
+            ),
+        dict(testcase_name='arbitrary values',
+             e_h_h = -5 + 3j,
+             e_h_v = +0.6 + 0.7j,
+             e_v_h = -2.1 - 0.8j,
+             e_v_v = 1.1 + 0.1j,
+             expected_m = (
+                20.56, 14.29, 8.83, -9.59,
+                18.49, 14.66, 7.37, -11.01,
+                -3.29, 1.49, -7.02, 2.81,
+                -4.63, -5.97, -4.79, -3.38)
+            ),
+        )
+    def test_single_values(self, e_h_h, e_h_v, e_v_h, e_v_v, expected_m):
+        '''
+        Testing single value inputs
+        '''
+        calculated_m = miop.mueller_matrix_ellipsoidal(e_h_h, e_h_v, e_v_h, e_v_v)
+        np.testing.assert_allclose(calculated_m, expected_m, atol=1e-7)
 
 if __name__ == '__main__':
     unittest.main()